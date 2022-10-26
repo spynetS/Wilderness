bfc48815ba5f468f4f0694330616e8ed545bf509
spynetS <alfred@stensatter.se>
2022-10-27 00:29:06 +0200

    update

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -2,14 +2,54 @@ package com.main;
 
 import com.javagamemaker.javagameengine.JavaGameEngine;
 import com.javagamemaker.javagameengine.Scene;
+import com.javagamemaker.javagameengine.components.Component;
 import com.javagamemaker.javagameengine.components.Sprite;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Main extends JavaGameEngine {
 
 
     public static void main(String[] args){
 
-        Scene scene1 = new Scene();
+        Scene scene1 = new Scene(){
+            @Override
+            public void load() {
+                try {
+                    FileInputStream fos = new FileInputStream("filename");
+                    ObjectInputStream oos = new ObjectInputStream(fos);
+
+                    Component c;
+                    for(Iterator var3 = ((LinkedList)oos.readObject()).iterator(); var3.hasNext();
+                        add(c)) {
+                        c = (Component)var3.next();
+                        if (c.getClass() == Sprite.class) {
+                            LinkedList<Rectangle[]> oldTiles = ((Sprite)c).tiles;
+                            ((Sprite)c).tiles = new LinkedList();
+                            ((Sprite)c).animations = new ArrayList();
+                            ((Sprite)c).animations1 = new ArrayList();
+                            Iterator var6 = oldTiles.iterator();
+
+                            while(var6.hasNext()) {
+                                Rectangle[] animation = (Rectangle[])var6.next();
+                                ((Sprite)c).loadAnimation(animation, ((Sprite)c).spriteSheetString);
+                            }
+
+                        }
+                    }
+
+                    fos.close();
+                } catch (Exception var8) {
+                    var8.printStackTrace();
+                }
+            }
+        };
+        scene1.load();
         scene1.setDebugMode(false);
         scene1.add(new Player());
 
--- src/com/main/Player.java
+++ src/com/main/Player.java
@@ -1,20 +1,26 @@
 package com.main;
 
-import com.javagamemaker.javagameengine.components.GameObject;
-import com.javagamemaker.javagameengine.components.Sprite;
+import com.javagamemaker.javagameengine.JavaGameEngine;
+import com.javagamemaker.javagameengine.components.*;
 import com.javagamemaker.javagameengine.input.Input;
 import com.javagamemaker.javagameengine.input.Keys;
+import com.javagamemaker.javagameengine.msc.Vector2;
+import jdk.jfr.Unsigned;
 
 import java.awt.*;
+import java.math.BigInteger;
 
 public class Player extends Sprite {
 
     public Player(){
 
+        //add(new Collider());
+       // add(new PhysicsBody());
         Rectangle[] up = new Rectangle[2];
         up[0] = new Rectangle(24,24,24,24);
         up[1] = new Rectangle(24,0,24,24);
         loadAnimation(up,"/ACharUp.png");
+
 
         up[0] = new Rectangle(24,24,24,24);
         up[1] = new Rectangle(24,0,24,24);
@@ -28,6 +34,7 @@ public class Player extends Sprite {
         up[1] = new Rectangle(24,0,24,24);
         loadAnimation(up,"/ACharLeft.png");
         setTimer(20);
+
     }
 
     @Override
@@ -35,15 +42,21 @@ public class Player extends Sprite {
         super.update();
         if(Input.isKeyDown(Keys.D)){
             animationIndex = 2;
+            setPosition(getPosition().add(Vector2.right));
         }
         if(Input.isKeyDown(Keys.A)){
             animationIndex = 3;
+            setPosition(getPosition().add(Vector2.left));
         }
         if(Input.isKeyDown(Keys.W)){
             animationIndex = 0;
+            setPosition(getPosition().add(Vector2.up));
         }
         if(Input.isKeyDown(Keys.S)){
             animationIndex = 1;
+            setPosition(getPosition().add(Vector2.down));
         }
+        JavaGameEngine.getSelectedScene().getCamera().setPosition(this.getFirstParent().getPosition().multiply(-1.0F).add(JavaGameEngine.getWindowSize().divide(2)));
+
     }
 }
